3bca487d8f1a7e10657847bcb71e224e45aca357
Konrad Biernacki <kgbier@gmail.com>
2017-11-30 10:49:28 +1100

    Implement functional webpack bundling

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,26 @@
+const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  target: 'node',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'umd',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015'],
+        },
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin(),
+  ]
 };
